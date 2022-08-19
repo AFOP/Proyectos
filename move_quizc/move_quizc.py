fc33f89b1bcea14099ca04a14e7e1151dd1c21d0
AFOP <4592@holbertonschool.com>
2022-08-18 21:26:05 -0500

    Update street

--- move_quizc/move_quizc.py
+++ move_quizc/move_quizc.py
@@ -11,19 +11,22 @@ speed = 0
 form = "O^o"
 print(Fore.YELLOW + "Welcome to the race Holberton 2022")
 print("=============================================")
-value = input(Fore.WHITE + "Please enter the speed of the bike: ")
-print(value)
+name = input(Fore.WHITE + "Name: ")
 print("=============================================")
-value = int(value)
-if (value > 15):
-    value = 15
-while (i < value):
+value = input(Fore.WHITE + "Please enter the speed of the bike: ")
+print("=============================================")
+value2 = int(value)
+if (value2 > 15):
+    value2 = 15
+while (i < value2):
     os.system("clear")
     speed += i
     spaces = (" " * speed)
-    street = ("===" * 40)
+    street = ("= =" * 40)
     time.sleep(0.1)
     print(Fore.YELLOW + f"{street}" + Fore.RESET)
     print(Fore.BLUE + f"{spaces} {form} {Fore.RESET}")
     print(Fore.YELLOW + f"{street}" + Fore.RESET)
-    i = i + 1 
+    i = i + 1
+
+print(Fore.YELLOW+"Thanks for participate {:s} your speed is: {:s}".format(name, value))
